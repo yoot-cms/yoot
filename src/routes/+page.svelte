0c82b5b679006aabbe4598a8f097000e084490b1
TheWisePigeon <zozozozeph@gmail.com>
2022-11-10 02:32:02 +0000

    fix: redirect users to auth page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
-<script lang="ts" >
-    
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    if (browser) {
+      if (!localStorage.getItem("token")) {
+        window.location.href = "/auth";
+      }
+    }
+  });
 </script>
 
-<div>
-    Welcome to Pigeon
-</div>
+<div>Welcome to Pigeon</div>
