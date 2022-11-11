1a43fd004c6b64d4f7cf4f5df35582517f977e03
TheWisePigeon <zozozozeph@gmail.com>
2022-11-11 21:03:14 +0000

    feat: added state management on user adding new container

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -2,7 +2,7 @@
   import axios from "axios";
   import { browser } from "$app/environment";
   import { onMount } from "svelte";
-
+  import { ContentContainers } from "../../stores/contentContainersStore"
   import Header from "../../components/Header.svelte";
   import ContentContainer from "../../components/ContentContainer.svelte";
   onMount(() => {
@@ -14,55 +14,10 @@
   });
 
   const url = "http://localhost:5000/container/";
-  const containers = [
-    {
-      name: "uh1",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-    {
-      name: "uh",
-    },
-  ];
 
   let containerName = "";
   let addingContainer = false;
   let error = false;
-  let errorMessage = "";
 
   function addContainer() {
     addingContainer = true
@@ -81,15 +36,43 @@
         }
       }
     ).then((res)=>{
+      const { data } = res.data
+      ContentContainers.update((containers)=>[
+        ...containers,
+        data
+      ])
       addingContainer = false
-      console.log(res.data)
+      containerName = ""
     }).catch((_err)=>{
       error = true
       addingContainer = false
     })
   }
+
+  async function getContainers(){
+    if (browser) {
+      await axios.get(
+        `${url}all`,
+        {
+          headers:{
+            Authorization: `Bearer ${localStorage.getItem("token")}`
+          }
+        }
+      ).then((res)=>{
+        const { data } = res.data
+        ContentContainers.set([...data])
+      }).catch((err)=>{
+        console.log(err.response)
+      })
+    }
+  }
+  let fetchresult = getContainers()
 </script>
 
+
+<svelte:head>
+	<title>YOOT | Dashboard</title>
+</svelte:head>
 <Header />
 <div class=" h-screen bg">
   <!-- Toolbar -->
@@ -132,9 +115,15 @@
   <div
     class=" text-white flex flex-wrap gap-12 h-4/5 border-t border-b p-3 border-transparent justify-center overflow-y-scroll list"
   >
-    {#each containers as container}
-      <ContentContainer name={container.name} />
-    {/each}
+  {#await fetchresult}
+    <div class=" flex justify-center items-center " >
+      <h1 class=" animate-pulse text-3xl "> Loading containers </h1>
+    </div>
+    {:then result} 
+      {#each $ContentContainers as container}
+        <ContentContainer name={container.name} />
+      {/each}
+    {/await}
   </div>
 </div>
 
