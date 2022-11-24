34dedf544046872b5bd9ba75402d6ee0831219dc
TheWisePigeon <zozozozeph@gmail.com>
2022-11-24 00:49:17 +0000

    ui: ui updates on entity builder and field component

--- src/components/Field.svelte
+++ src/components/Field.svelte
@@ -9,18 +9,35 @@
     }
 </script>
 
-<div class=" flex justify-around my-3 text-center text-white" >
+<div class=" flex justify-around my-3 text-center text-white border-b-2 border-dotted" >
     <div class=" w-64">
         {name}
     </div>
-    <div class=" w-64">
-        {type}
+    <div class=" w-64 flex gap-2 justify-center">
+        <h1>
+            {type}
+        </h1>
+        <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="w-5 h-5 cursor-pointer"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM8.94 6.94a.75.75 0 11-1.061-1.061 3 3 0 112.871 5.026v.345a.75.75 0 01-1.5 0v-.5c0-.72.57-1.172 1.081-1.287A1.5 1.5 0 108.94 6.94zM10 15a1 1 0 100-2 1 1 0 000 2z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </button>
     </div>
     <div class=" flex justify-center  w-64">
        <button on:click={removeField} class="" >
          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={` hover:scale-105 hover:-translate-y-1 transition-all hover:text-red-500 w-6 h-6`}>
              <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
          </svg>  
-       </button>        
+       </button>       
+        
     </div>
 </div>
