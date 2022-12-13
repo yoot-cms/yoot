1ed4651ec50d241a5aa657d9370723fd5e49792d
TheWisePigeon <zozozozeph@gmail.com>
2022-12-13 20:43:41 +0000

    ui: added mobile screen sizes landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,43 @@
-
 <svelte:head>
   <title>YOOT | Minimalistic CMS</title>
-  <meta name="description" content="YOOT is a simple to use minimalistic CMS, designed for everyone running a content based business, dev or non dev YOOT is YOyoursOrs">
+  <meta
+    name="description"
+    content="YOOT is a simple to use minimalistic CMS, designed for everyone running a content based business, dev or non dev YOOT is YOyoursOrs"
+  />
 </svelte:head>
 
-<div class=" flex justify-around items-center my-3" >
-  <div>
-    <h1 class=" text-2xl font-extrabold" >YOOT</h1>
-  </div>
-  <div class=" flex justify-between gap-10" >
-    <button class=" cta">Docs</button>
-    <button class=" cta">Get started</button>
-  </div>
-</div>
 
-<div class=" text-center text-3xl mt-14" >
-  <h1 class=" font-extrabold" >
-    Content Management the <h1 >EASY way</h1>
-  </h1>
-</div>
-<h1 class=" text-center text-2xl my-5">
-  Manage your content driven websites easily and without the need of developers
-</h1>
-<div class=" flex justify-center" >
-  <a href="/auth" class="cta my-5 " >Get started</a>
+<div class=" p-5 " >
+  
+  <!-- Nav -->
+  <div class="  flex justify-between items-center ">
+    <div
+      class=" bg-no-repeat bg-cover bg-center bg-[url('/logo.png')] h-10 w-10"
+    />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>  
+  </div>
+  
+  <!-- Description -->
+  <div class=" mt-10" >
+    <h1 class=" font-extrabold text-3xl" >
+      Manage your content as easily as 1-2-3
+    </h1>
+    <p class=" mt-10 text-gray-500 text-lg" >
+      YOOT is a simple and intuitive CMS designed for developers and business owners alike. 
+      With its user-friendly interface and powerful features, YOOT makes it easy to manage and update your website content without any technical expertise. 
+      Try YOOT today and experience the benefits of seamless content management for yourself!
+    </p>
+  </div>
+
+  <!-- Call to action -->
+  <div class=" mt-10 flex gap-3" >
+    <button class=" rounded-md p-2 bg-indigo-600 text-white" >
+      Get Started
+    </button>
+    <button class=" rounded-md border border-gray-400 bg-transparent p-2 text-slate-500" >
+      Docs
+    </button>
+  </div>
 </div>
