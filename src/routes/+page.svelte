f53a25028e485f7028716944f6d234e70c706cf7
TheWisePigeon <zozozozeph@gmail.com>
2022-12-13 22:25:09 +0000

    ui: added large screens landing page hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,25 +14,29 @@
     <div
       class=" bg-no-repeat bg-cover bg-center bg-[url('/logo.png')] h-10 w-10"
     />
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>  
+    <div class=" flex justify-between gap-3 md:gap-6 lg:gap-10" >
+      <a class=" hover:underline" href="#pricing">Pricing</a>
+      <a class=" hover:underline" href="/docs">Docs</a>
+      <a class=" hover:underline" href="/auth">Login</a>
+    </div> 
   </div>
   
   <!-- Description -->
-  <div class=" mt-10" >
-    <h1 class=" font-extrabold text-3xl" >
+  <div class=" mt-10 md:text-center" >
+    <h1 class=" font-extrabold text-3xl " >
       Manage your content as easily as 1-2-3
     </h1>
-    <p class=" mt-10 text-gray-500 text-lg" >
-      YOOT is a simple and intuitive CMS designed for developers and business owners alike. 
-      With its user-friendly interface and powerful features, YOOT makes it easy to manage and update your website content without any technical expertise. 
-      Try YOOT today and experience the benefits of seamless content management for yourself!
-    </p>
+    <div class="lg:w-[48rem] lg:m-auto " >
+        <p class=" mt-10 text-gray-500 text-lg " >
+          YOOT is a simple and intuitive CMS designed for developers and business owners alike. 
+          With its user-friendly interface and powerful features, YOOT makes it easy to manage and update your website content without any technical expertise. 
+          Try YOOT today and experience the benefits of seamless content management for yourself!
+        </p>
+    </div>
   </div>
 
   <!-- Call to action -->
-  <div class=" mt-10 flex gap-3" >
+  <div class=" mt-10 flex gap-3 md:justify-center" >
     <button class=" rounded-md p-2 bg-indigo-600 text-white" >
       Get Started
     </button>
