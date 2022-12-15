940dc4cb2e85c53167c532974a3636801e32d0b4
TheWisePigeon <zozozozeph@gmail.com>
2022-12-15 18:11:02 +0000

    ui: added entities page layout

--- src/routes/containers/+page.svelte
+++ src/routes/containers/+page.svelte
@@ -16,7 +16,7 @@
   <!-- Toolbar -->
   <div class=" h-[5%] flex justify-between items-center" >
     <h1>You are viewing your Containers</h1>
-    <button class=" flex border-slate-400 border  rounded-md p-1 bg-indigo-500 text-white " >
+    <button class=" flex border rounded-md p-1 bg-indigo-500 text-white " >
       <h1>Add container</h1>     
     </button>
   </div>
@@ -24,7 +24,7 @@
   <section class={`border-[3px] border-spacing-10 border-dashed border-slate-400 rounded-md h-[95%] w-full ${false?'grid grid-cols-5':'flex '} p-2 gap-5 overflow-y-scroll`} >
     <div class={`${true?"":"hidden"} flex flex-col h-full w-full items-center justify-center`} >
       <h1>You don't have any content container</h1> 
-      <button class=" flex border-slate-400 border  rounded-md p-1 bg-indigo-500 text-white " >
+      <button class=" flex border  rounded-md p-1 bg-indigo-500 text-white " >
         <h1>Add Container</h1>
       </button>
     </div>
--- src/routes/containers/entities/+page.svelte
+++ src/routes/containers/entities/+page.svelte
@@ -11,4 +11,35 @@
     <title>YOOT | Entities</title>
 </svelte:head>
 
-<h1>Welcome to your en TiTies</h1>
+<main class=" h-full flex justify-between" >
+  <!-- Entities list -->
+  <section class=" h-full w-[55%] flex flex-col " >
+    
+    <!-- Title and button section -->
+    <div class=" flex justify-between items-center h-[5%]" >
+      <h1>You are viewing Entities in <span class=" font-bold" >Container</span> </h1>
+      <button class=" flex border  rounded-md p-1 bg-indigo-500 text-white " >
+        <h1>Add Entity</h1>
+      </button>
+    </div>
+
+    <!-- Actual list -->
+    <div class=" h-[95%] border-[3px] border-dashed border-slate-400 rounded-md w-full flex flex-col justify-center items-center" >
+      <h1>Seems like you don't have any entities yet</h1>
+      <button class=" flex border  rounded-md p-1 bg-indigo-500 text-white " >
+        <h1>What about creating one?</h1>
+      </button>
+    </div>
+  </section>
+
+  <!-- Toolbar -->
+  <section class=" h-full flex flex-col w-[30%]" >
+    <!-- Search bar -->
+    <div class=" flex justify-end items-center h-[5%]" >
+    </div>
+
+    <!-- Toolbar utilities -->
+    <div class=" h-[95%] border-[3px] border-dashed rounded-md border-slate-400" >      
+    </div>
+  </section>
+</main>
