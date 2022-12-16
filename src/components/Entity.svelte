c6862b15659291491fa9fd79c45612076a53529d
TheWisePigeon <zozozozeph@gmail.com>
2022-12-16 14:45:04 +0000

    ui: added entity component

---
+++ src/components/Entity.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+  const list = [
+    {
+      name: "Profile",
+      type: "Text",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+    {
+      name: "Profile picture",
+      type: "Media",
+    },
+  ];
+
+  $: toggleFields = false;
+</script>
+
+<div
+  class=" bg-grayscale-400 p-3 rounded-md text-white cursor-pointer flex justify-between h-12"
+>
+  <div>
+    <h1>Entity name</h1>
+  </div>
+
+  <div class="relative flex flex-col items-center">
+    <button
+      on:click={() => {
+        toggleFields = !toggleFields;
+      }}
+      class=" flex items-center"
+    >
+      <h1 class="">Fields</h1>
+      {#if toggleFields}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      {:else}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="w-5 h-5"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      {/if}
+    </button>
+    <!-- Fields -->
+    <div
+      class={` ${
+        toggleFields ? "" : "hidden"
+      } rounded-md p-2  bg-slate-400 w-64 cursor-default z-10`}
+    >
+      {#each list as item}
+        <div class=" flex justify-between gap-2 border-b-[1px] ">
+          <div class=" w-[50%] overflow-hidden truncate text-center ">
+            <h1>{item.name.slice(0, 9)}{item.name.length > 10 ? "..." : ""}</h1>
+          </div>
+          <div class="">
+            <h1>:</h1>
+          </div>
+          <div class=" w-[50%] text-center ">
+            <h1>{item.type}</h1>
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+
+  <div>
+    <h1>15 total ressources</h1>
+  </div>
+</div>
--- src/routes/containers/entities/+page.svelte
+++ src/routes/containers/entities/+page.svelte
@@ -4,7 +4,7 @@
   if (browser) {
     location.update(_value=>window.location.pathname);
   }
-
+  import Entity from "../../../components/Entity.svelte";
 </script>
 
 <svelte:head>
@@ -18,17 +18,22 @@
     <!-- Title and button section -->
     <div class=" flex justify-between items-center h-[5%]" >
       <h1>You are viewing Entities in <span class=" font-bold" >Container</span> </h1>
-      <button class=" flex border  rounded-md p-1 bg-indigo-500 text-white " >
+      <button class=" px-2 flex border  rounded-md p-1 bg-indigo-500 text-white " >
         <h1>Add Entity</h1>
       </button>
     </div>
 
     <!-- Actual list -->
-    <div class=" h-[95%] border-[3px] border-dashed border-slate-400 rounded-md w-full flex flex-col justify-center items-center" >
-      <h1>Seems like you don't have any entities yet</h1>
-      <button class=" flex border  rounded-md p-1 bg-indigo-500 text-white " >
+    <div class={` p-3 h-[95%] overflow-y-scroll border-[3px] border-dashed border-slate-400 rounded-md w-full ${ true?" flex flex-col gap-5 ":"flex flex-col justify-center items-center"}`} >
+      <!-- <h1>Seems like you don't have any entities yet</h1>
+      <button class="px-2 flex border  rounded-md p-1 bg-indigo-500 text-white " >
         <h1>What about creating one?</h1>
-      </button>
+      </button> -->
+      <Entity/>
+      <Entity/>
+      <Entity/>
+      <Entity/>
+      <Entity/>
     </div>
   </section>
 
