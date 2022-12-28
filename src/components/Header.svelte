be805f37387d193cb62cc1400ecf6beb7e7e93d4
TheWisePigeon <zozozozeph@gmail.com>
2022-12-28 18:02:55 +0000

    ui: added basic header

---
+++ src/components/Header.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" >
+
+</script>
+
+<main>
+    <header class="header" >
+        <h1 class=" font-bold text-lg" >YOOT</h1>
+        <div class="flex gap-5" >
+            <h1 class=" underline">Docs</h1>
+            <h1 class=" underline">About</h1>
+        </div>
+    </header>
+</main>
+
+<style>
+    .header{
+        @apply flex justify-between items-center p-2 border-b-2;
+    }
+</style>
