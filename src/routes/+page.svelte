4c16f220267192c81ea9e58e556f4b072c79c1de
TheWisePigeon <zozozozeph@gmail.com>
2023-01-28 10:50:59 +0000

    ui: added landing page concept

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,52 @@
 <script lang="ts" >
 </script>
 
-<header class=" flex justify-between h-20 items-center shadow-xl p-3 " >
-  <h1 class=" text-xl" >YOOT</h1>
+<header class=" z-50 flex justify-between h-18 items-center shadow-md p-3 w-full sticky top-0 bg-white" >
+  <a href="/" class=" text-xl" >YOOT</a>
   <div class=" flex gap-10" >
     <a href="/docs" class="underline" >Docs</a>
-    <a href="/auth" class="underline" >Login</a>
+    <a href="/auth" class="underline" >Get started</a>
   </div>
 </header>
+
+<h1 class=" p-3 mt-5 text-2xl font-extrabold text-gray-700" >
+  Manage your content as easily as <a class="link_to_feature" href="#one">one</a>, <a class="link_to_feature" href="#two">two</a>, <a class="link_to_feature" href="#three">three</a>
+</h1>
+
+<section class="p-3 w-full" >
+  <h1 class=" text-xl font-extrabold " >1. Create an Entity</h1>
+  <h1>
+    Use YOOT's intuitive 
+    <a href="https://v1" class=" text-violet-500 " >
+      <h1 class=" inline" >Entity builder</h1>
+    </a> 
+    to quicky describe your ressources schema
+  </h1>
+  <!-- Frame to contain demo gif -->
+  <div class="w-full border border-slate-500 h-[20rem]" >
+
+  </div>
+</section>
+
+<section class="p-3 w-full" >
+  <h1 class=" text-xl font-extrabold " >2. Populate it</h1>
+  <h1>
+    Now use the Ressource manager to create, edit or delete ressources in that Entity
+  </h1>
+  <!-- Frame to contain demo gif -->
+  <div class="w-full border border-slate-500 h-[20rem]" >
+
+  </div>
+</section>
+
+<section class="p-3 w-full" >
+  <h1 class=" text-xl font-extrabold " >3. Yoot your website</h1>
+  <h1>
+    Integrate YOOT into your website using our <a class=" underline text-violet-500" href="#docs">REST api</a> or <a class="underline text-violet-500" href="#docs">NPM package</a> 
+  </h1>
+  <h1>If you are not a developer, you might need help for this one. <a href="mailto:zozozozeph@gmail.com" class=" text-violet-500" >Contact us</a> us and we will answer all your questions.</h1>
+</section>
+
+<footer class="text-xs flex items-center p-3 border-t border space-y-2" >
+  <h1  >YOOT by <a href="https://github.com/Xerxes-Labs">Xerxes Labs</a> 2023 All rights reserved</h1>
+</footer>
