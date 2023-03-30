e8a29442ebebb189b51c30f7fcf4490b8b10a930
TheWisePigeon <zozozozeph@gmail.com>
2023-03-30 21:16:55 +0000

    ref: removed header from layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
+<script lang="ts">
+</script>
 
+<main class=" p-2 w-full">
+	<!-- Hero section -->
+	<section class=" w-full">
+		<h1 class=" font-extrabold text-4xl text-purple-800">
+			Manage your content as easily as One Two Three
+		</h1>
+	</section>
+</main>
