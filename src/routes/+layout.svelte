d6eac1f2e4ec3972fc1ff42640c4c343017aadb0
TheWisePigeon <zozozozeph@gmail.com>
2023-03-30 20:32:49 +0000

    feat: add header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,15 @@
     import "../app.css"
 </script>
 
+<header class="p-2 sticky top-0 flex w-full justify-between" >
+    <a href="/">YOOT</a>
+    <div class=" flex gap-5" >
+        <h1>Docs</h1>
+        <h1>About</h1>
+        <h1>Developers</h1>
+    </div>
+</header>
+
+<div class=" h-screen w-full" ></div>
 
 <slot/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1 class=" bg-fuchsia-400" >Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
