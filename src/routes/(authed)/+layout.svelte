bce411813c346d2479ca82bcc62fe8c8a9e4acf1
TheWisePigeon <zozozozeph@gmail.com>
2023-04-05 14:32:08 +0000

    minor changes

---
+++ src/routes/(authed)/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+
+	onMount(() => {
+		if (browser) {
+			const auth_token = localStorage.getItem('auth_token') ?? '';
+			if (auth_token === '') {
+				goto('/auth');
+			}
+			//Check if token is valid
+			//Make call to token validation API
+		}
+	});
+</script>
+
+<slot />
---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts" >
+</script>
+
+<div>
+    Welcome to auth bitch
+</div>
