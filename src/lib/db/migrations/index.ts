21bd61be299857c36f94956fa24d7fd336a54d48
TheWisePigeon <zozozozeph@gmail.com>
2023-04-06 11:24:35 +0000

    feat: add Migration type alias

--- src/lib/db/migrations/index.ts
+++ src/lib/db/migrations/index.ts
@@ -1,4 +1,6 @@
+import type { Migration } from "$lib/types";
 
-export function run(){
+export function run( migration: Migration ){
+    
 
 }
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,1 +1,9 @@
 export type Log = "WARN" | "ERR" | "INFO"
+
+
+export type Migration = {
+    version: number,
+    description? : string,
+    keyspace: string,
+    tables: Array<Record<string, unknown>> 
+}
