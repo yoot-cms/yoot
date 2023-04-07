aee073fbf5b973f0318af026069ff13e45d69d3b
TheWisePigeon <zozozozeph@gmail.com>
2023-04-07 19:08:57 +0000

    ref: minor changes

--- src/routes/(authed)/+layout.svelte
+++ src/routes/(authed)/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-/**
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
@@ -10,11 +9,8 @@
 			if (auth_token === '') {
 				goto('/auth');
 			}
-			//Check if token is valid
-			//Make call to token validation API
 		}
 	});
-    **/
 </script>
 
 <slot />
