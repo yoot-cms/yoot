dda6b044de057d09be5faed050605dfa562c76d8
TheWisePigeon <zozozozeph@gmail.com>
2023-04-07 13:09:01 +0000

    add base auth page

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,6 +1,14 @@
-<script lang="ts" >
+<script lang="ts">
 </script>
 
-<div>
-    Welcome to auth bitch
+<div class=" h-screen w-full flex flex-col items-center justify-center p-2 ">
+	<div class=" w-full h-52 flex flex-col gap-5">
+		<h1 class=" text-center text-3xl font-bold">Welcome (back)</h1>
+        <h1 class=" text-center">Enter your email below and we will send you a code to authenticate</h1>
+		<form class=" flex flex-col items-center gap-2">
+			<input type="email" class=" p-2 focus:outline-none rounded-md w-full border border-gray-500" placeholder="Your Email" required />
+
+			<button class=" p-2 rounded-md border border-white text-white font-semibold bg-violet-700 w-full"> Get code </button>
+		</form>
+	</div>
 </div>
