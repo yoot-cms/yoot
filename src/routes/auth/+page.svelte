a30e20a453b5e917f2519a96c2c9150cf995e1f6
TheWisePigeon <zozozozeph@gmail.com>
2023-04-07 16:43:31 +0000

    a little bit of refactoring

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -13,37 +13,37 @@
 			const req_url = new URL('/api/auth/request');
 			req_url.searchParams.append('email', email);
 			const auth_request_response = await fetch(req_url);
-            if(auth_request_response.status!==200){
-                    error_sending_code = "Something went wrong. Please retry or contact support"
-                    return
-                }
-            error_sending_code = ""
-            auth_state = 1
+			if (auth_request_response.status !== 200) {
+				error_sending_code = 'Something went wrong. Please retry or contact support';
+				return;
+			}
+			error_sending_code = '';
+			auth_state = 1;
 		} catch (_) {
-			error_sending_code = "Something went wrong. Please retry or contact support";
+			error_sending_code = 'Something went wrong. Please retry or contact support';
 		}
 	}
 
-    async function submit_code(){
-            try {
-                const req_url = new URL('/api/auth/verify')
-                req_url.searchParams.append("email", email)
-                req_url.searchParams.append("code", auth_code)
-                const code_submit_response = await fetch(req_url)
-                if(code_submit_response.status!==200 && code_submit_response.status!==404){
-                        error_submitting = "Something went wrong. Please retry or contact support"
-                        return
-                    }                    
-                if(code_submit_response.status===404){
-                        error_submitting = "Invalid code"
-                        return
-                    }
-                const data = await code_submit_response.json()
-                console.log(data)
-            } catch (err) {
-                error_submitting = "Something went wrong. Please retry or contact support"
-            }
-        }
+	async function submit_code() {
+		try {
+			const req_url = new URL('/api/auth/verify');
+			req_url.searchParams.append('email', email);
+			req_url.searchParams.append('code', auth_code);
+			const code_submit_response = await fetch(req_url);
+			if (code_submit_response.status !== 200 && code_submit_response.status !== 404) {
+				error_submitting = 'Something went wrong. Please retry or contact support';
+				return;
+			}
+			if (code_submit_response.status === 404) {
+				error_submitting = 'Invalid code';
+				return;
+			}
+			const data = await code_submit_response.json();
+			console.log(data);
+		} catch (err) {
+			error_submitting = 'Something went wrong. Please retry or contact support';
+		}
+	}
 </script>
 
 <div class=" h-screen w-full flex flex-col items-center justify-center p-2">
@@ -53,7 +53,7 @@
 			<h1 class=" text-center">
 				Enter your email below and we will send you a code to authenticate
 			</h1>
-			<form class=" flex flex-col items-center gap-2">
+			<form on:submit|preventDefault={request_auth} class=" flex flex-col items-center gap-2">
 				<input
 					type="email"
 					class=" p-2 focus:outline-none rounded-md w-full border border-gray-500 text-center"
@@ -63,6 +63,7 @@
 				/>
 				<span class=" text-red-700 text-sm">{error_sending_code}</span>
 				<button
+					type="submit"
 					class=" flex justify-center p-2 rounded-md border border-white text-white font-semibold bg-violet-700 w-full"
 				>
 					{#if sending_code}
@@ -75,7 +76,7 @@
 		{/if}
 		{#if auth_state === 1}
 			<h1 class=" text-center">Check your mail box and enter the 6 digit code you received</h1>
-			<form class=" flex flex-col items-center gap-2">
+			<form on:submit|preventDefault={submit_code} class=" flex flex-col items-center gap-2">
 				<input
 					type="text"
 					class=" p-2 focus:outline-none rounded-md w-full border border-gray-500 text-center"
@@ -85,7 +86,7 @@
 					bind:value={auth_code}
 				/>
 				<span class="text-red-700 text-sm">{error_submitting}</span>
-				<button class=" p-2 rounded-md text-white font-semibold bg-violet-700 w-full">
+				<button type="submit" class=" p-2 rounded-md text-white font-semibold bg-violet-700 w-full">
 					{#if submitting}
 						<Spinner />
 					{:else}
