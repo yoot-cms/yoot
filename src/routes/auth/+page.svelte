16de6fdf4a76ef757fc4b8316163cc878e23bfd1
TheWisePigeon <zozozozeph@gmail.com>
2023-04-07 16:13:01 +0000

    add auth code submition screen

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	let auth_state = 0;
+	let auth_state = 1;
 </script>
 
 <div class=" h-screen w-full flex flex-col items-center justify-center p-2">
@@ -24,8 +24,17 @@
 				</button>
 			</form>
 		{/if}
-        {#if auth_state===1}
-            <h1>Something</h1>
-        {/if}
+		{#if auth_state === 1}
+			<h1 class=" text-center">Check your mail box and enter the 6 digit code you received</h1>
+			<form class=" flex flex-col items-center gap-2">
+				<input
+					type="text"
+					class=" p-2 focus:outline-none rounded-md w-full border border-gray-500 text-center"
+					placeholder=""
+					required
+                    maxlength="6"
+				/>
+			</form>
+		{/if}
 	</div>
 </div>
