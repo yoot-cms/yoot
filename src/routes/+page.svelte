b4194701391bfa76d2e99447185b93ae75c18548
TheWisePigeon <zozozozeph@gmail.com>
2023-04-07 19:58:49 +0000

    ref: format code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,3 @@
-<script></script>
-
 <header class=" flex w-full justify-between p-2 fixed top-0">
 	<h1 class=" font-bold">YOOT</h1>
 	<div class=" flex gap-5 underline">
@@ -9,19 +7,23 @@
 </header>
 <main class="p-2 h-screen w-full">
 	<div class=" h-[100%] w-full flex gap-2 flex-col justify-center items-center">
-	    <h1 class=" text-5xl text-center font-bold">Content Management made Simple</h1>	
-        <h1 class=" text-center text-gray-500">YOOT is a minimalistic CMS built with simplicity in mind. It is suitable for everyone owning a content driven business, content creators and developers building content driven apps</h1>
-        <div class=" flex justify-center mt-10 gap-2 w-full ">
-            <button class=" p-2 rounded-md bg-violet-500 text-white">
-                Get Started
-            </button>
-            <button class=" p-2 rounded-md text-gray-500 border border-gray-500">
-                Learn More
-            </button>
-        </div>
+		<h1 class=" text-5xl text-center font-bold">Content Management made Simple</h1>
+		<h1 class=" text-center text-gray-500">
+			YOOT is a minimalistic CMS built with simplicity in mind. It is suitable for everyone owning a
+			content driven business, content creators and developers building content driven apps
+		</h1>
+		<div class=" flex justify-center mt-10 gap-2 w-full">
+			<button class=" p-2 rounded-md bg-violet-500 text-white"> Get Started </button>
+			<button class=" p-2 rounded-md text-gray-500 border border-gray-500"> Learn More </button>
+		</div>
 	</div>
 </main>
-<footer class="flex w-full justify-center border-t border-gray-500 p-1" >
-    <h1 class="text-center text-sm fixed bottom-0" >Built with ❣️  by  developers <a  class="text-violet-500"  href="https://reexlabs.com" target="_blank" >@REEX Labs</a>  </h1>
-
+<footer class="flex w-full justify-center border-t border-gray-500 p-1">
+	<h1 class="text-center text-sm fixed bottom-0">
+		Built with ❣️ by developers <a
+			class="text-violet-500"
+			href="https://reexlabs.com"
+			target="_blank">@REEX Labs</a
+		>
+	</h1>
 </footer>
