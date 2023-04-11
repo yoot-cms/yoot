e5ac330156f60223faa2bf87fd8f6c4a5d8468b1
TheWisePigeon <zozozozeph@gmail.com>
2023-04-11 20:25:20 +0000

    feat: add skeleton ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
-<script lang="ts" >
-    import "../app.css"
+<script lang="ts">
+	// Your selected Skeleton theme:
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+	// This contains the bulk of Skeletons required styles:
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
+	import '../app.postcss';
 </script>
 
 <slot />
