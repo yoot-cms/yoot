88d788e1e02f9bb24f43804c1bd7d32498442625
TheWisePigeon <zozozozeph@gmail.com>
2023-04-11 20:30:40 +0000

    feat: add redirect to auth page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+</script>
+
 <header class=" flex w-full justify-between p-2 fixed top-0">
 	<h1 class=" font-bold">YOOT</h1>
 	<div class=" flex gap-5 underline">
@@ -13,8 +17,8 @@
 			content driven business, content creators and developers building content driven apps
 		</h1>
 		<div class=" flex justify-center mt-10 gap-2 w-full">
-			<button class=" p-2 rounded-md bg-violet-500 text-white"> Get Started </button>
-			<button class=" p-2 rounded-md text-gray-500 border border-gray-500"> Learn More </button>
+			<button on:click={()=>{ goto("/auth") }} class=" p-2 rounded-md bg-violet-500 text-white"> Get Started </button>
+			<a href="https://yoot.reexlabs.com/docs" class=" p-2 rounded-md text-gray-500 border border-gray-500"> Learn More </a>
 		</div>
 	</div>
 </main>
