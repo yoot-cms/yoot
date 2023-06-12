c0377e0eaf6e0c74fa189e3456129c1061e68e9a
TheWisePigeon <zozozozeph@gmail.com>
2023-06-12 14:44:19 +0000

    minor updates to auth

--- src/routes/api/auth/register/+server.ts
+++ src/routes/api/auth/register/+server.ts
@@ -3,17 +3,20 @@ import sql from "$lib/db"
 import bcrypt from "bcrypt"
 
 export const POST = (
-    async ({ request })=>{
+    async ({ request }) => {
         try {
-            const { email, password } = await request.json() as { email:string, password:string }
+            const { email, password, username } = await request.json() as { email: string, password: string, username: string }
             const result = await sql`select email from users where email=${email}`
-            if(result.rows.length!==0) return new Response("", { status:409 })
+            if (result.rows.length !== 0) return new Response("", { status: 409 })
             const hashed_pwd = await bcrypt.hash(password, 10)
-            await sql`insert into users(email, password) values(${email}, ${hashed_pwd})`
-            return new Response("")
+            await sql`
+                insert into users(email, password, username) 
+                values(${email}, ${hashed_pwd}, ${username})
+            `
+            return new Response("", { status:201 })
         } catch (err) {
             console.log(err)
-            return new Response("", { status:500 })
+            return new Response("", { status: 500 })
         }
     }
 ) satisfies RequestHandler
