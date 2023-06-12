d69d59afb11735b987387df8533aaacb1c25cb9b
TheWisePigeon <zozozozeph@gmail.com>
2023-06-12 13:55:52 +0000

    migrations: create entity table

--- src/lib/db/migrations/v1/up.sql
+++ src/lib/db/migrations/v1/up.sql
@@ -1,11 +1,20 @@
 create table users (
     id text default gen_random_uuid()::text PRIMARY KEY,
     email text not null,
-    password varchar(255) not null
+    password varchar(255) not null,
+    username text not null,
+    plan text not null
 );
 
 create table project (
     id text default gen_random_uuid()::text PRIMARY KEY,
     name text not null,
     owner text REFERENCES users(id)
+);
+
+create table entity (
+    id text default gen_random_uuid()::text PRIMARY KEY,
+    name text not null,
+    project text REFERENCES project(id),
+    schema JSONB[]
 );
