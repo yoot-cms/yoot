590c16d52102e01eac37b083a59a0bc36e9041df
TheWisePigeon <zozozozeph@gmail.com>
2023-06-12 13:45:34 +0000

    migrations: create project table

--- src/lib/db/migrations/v1/up.sql
+++ src/lib/db/migrations/v1/up.sql
@@ -4,8 +4,8 @@ create table users (
     password varchar(255) not null
 );
 
-create table projects (
+create table project (
     id text default gen_random_uuid()::text PRIMARY KEY,
     name text not null,
-    owner text not null
+    owner text REFERENCES users(id)
 );
