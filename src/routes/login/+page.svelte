f75495f096422ae9ab13de86a4dd12e2abcb8ebf
TheWisePigeon <zozozozeph@gmail.com>
2023-06-14 15:30:36 +0000

    ui: add login basic ui

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="fixed flex h-screen w-screen items-center justify-center bg-white">
+	<div
+		class="max-h-[75vh] lg:max-h-[60vh] max-w-[90vw] rounded-lg p-4 transition-all md:max-w-[75vw] lg:max-w-[60vw] duration-500"
+	>
+        <form class=" flex flex-col gap-5 ">
+            <input class="border-2 border-gray-500 focus:outline-none p-2 rounded-md" type="email" required placeholder="Enter your email">
+            <input class="border-2 border-gray-500 focus:outline-none p-2 rounded-md" type="password" required placeholder="Enter your password">
+            <button class="p-2 bg-blue-500 text-white rounded-md">
+                <h1>Login</h1>
+            </button>
+            <a class="text-sm text-blue-500 underline" href="/register">Don't have an account ? Register instead</a>
+        </form>
+	</div>
+</div>
