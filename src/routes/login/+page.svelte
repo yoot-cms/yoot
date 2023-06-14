a1617e7ab32b3f602a492d8d46485a854047961d
TheWisePigeon <zozozozeph@gmail.com>
2023-06-14 16:33:25 +0000

    minor updates

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
+    import Loading from "$lib/components/Loading.svelte";
 	let email = '';
 	let password = '';
+    let loading = false
+    let error = ""
 	async function login() {
+        loading = true
+        error = ""
 		try {
 			const response = await fetch('http://localhost:5173/api/auth/login', {
 				method: 'POST',
 				body: JSON.stringify({ email, password })
 			});
+            loading = false
             if (response.status===500) {
-                alert("Something went wrong. Please try again or contact us")
+                error = "Something went wrong. Please try again or contact us"
             }
             if (response.status===400) {
-                alert("Invalid credentials")
+                error = "Invalid credentials"
             }
             if (response.status===200) {
                 const { token } = await response.json() as { token : string }
@@ -42,8 +48,13 @@
 				required
 				placeholder="Enter your password"
 			/>
-			<button class="p-2 bg-blue-500 text-white rounded-md">
-				<h1>Login</h1>
+            <h1 class=" text-xs text-red-500 text-center">{error}</h1>
+			<button disabled={loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
+                {#if !loading}
+                    <h1>Login</h1>
+                {:else}
+                    <Loading/>
+                {/if}
 			</button>
 			<a class="text-sm text-blue-500 underline" href="/register"
 				>Don't have an account ? Register instead</a
