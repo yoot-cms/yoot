8977c8c11257d0089642678e824e03759b51e526
TheWisePigeon <zozozozeph@gmail.com>
2023-06-19 20:31:39 +0000

    ui updates

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,6 +3,7 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
     import { API_URL } from "$lib/utils"
+    import Loading_1 from '$lib/components/Loading_1.svelte';
 	onMount(async () => {
 		if (browser) {
 			const token = localStorage.getItem('token') ?? '';
@@ -33,5 +34,12 @@
 		</div>
 	</div>
 {:else}
-    <h1>Loading</h1>
+	<div class=" p-1 relative h-screen w-full">
+		<div
+			class=" p-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-md flex flex-col items-center justify-center"
+		>
+			<Loading_1/>
+			<div />
+		</div>
+	</div>
 {/if}
