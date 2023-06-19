790d2fbedcfef0964215a1460fdb4ccf800b2b3f
TheWisePigeon <zozozozeph@gmail.com>
2023-06-19 20:23:04 +0000

    ui updates

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const API = "http://localhost:5173/api"
+
---
+++ src/routes/api/user/info/+server.ts
@@ -0,0 +1,18 @@
+import { HttpResponse } from "$lib";
+import type { RequestHandler } from "./$types";
+import { verify_token } from "$lib";
+import sql from "$lib/db"
+
+export const GET : RequestHandler = async ({ request })=>{
+    try {
+        const token = request.headers.get("Authorization") ?? ""
+        if (token === "") return HttpResponse(401)
+        const { status, id } = verify_token(token)
+        if (!status) return HttpResponse(401)
+        const { rows } = await sql` SELECT username, email FROM users WHERE id=${id} `
+        return HttpResponse(200, JSON.stringify({ data: rows[0] }))
+    } catch (err) {
+        console.log(err)
+        return HttpResponse(500)
+    }
+}
