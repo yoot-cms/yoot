37c0bbcc1eacf32131566b703c488da0367d1309
TheWisePigeon <zozozozeph@gmail.com>
2023-06-19 18:04:56 +0000

    ref: cleaned a bit

--- src/routes/api/admin/projects/+server.ts
+++ src/routes/api/admin/projects/+server.ts
@@ -5,9 +5,8 @@ import { HttpResponse } from "$lib";
 export const GET = (
      async ()=>{
         try {
-            const{ rows } = await sql `select * from project`
+            const { rows } = await sql `select * from project`
             return HttpResponse(200, JSON.stringify({ data: rows }))
-
         } catch (err) {
             console.log(err)
             return HttpResponse(500)
--- src/routes/api/admin/projects/[id]/entity/+server.ts
+++ src/routes/api/admin/projects/[id]/entity/+server.ts
@@ -1,14 +1,13 @@
 import type { RequestHandler } from "./$types";
 import { HttpResponse } from "$lib";
-import { verify_token } from "$lib";
 import sql from "$lib/db"
 
 export const GET = (
-     async ({ request, params})=>{
+     async ({ params})=>{
         try {
             
-            const {id} = params
-            const {rows} = await sql `SELECT * FROM entity WHERE project = ${id}`
+            const { id } = params
+            const { rows } = await sql `SELECT * FROM entity WHERE project = ${id}`
             return HttpResponse(200, JSON.stringify({ data: rows }))
             
         } catch (err) {
