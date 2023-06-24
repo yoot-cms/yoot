ed379300e3491bc15100e31624cad950b5d2db03
TheWisePigeon <zozozozeph@gmail.com>
2023-06-24 23:05:24 +0000

    ui: add sidebar item mock component

--- src/lib/components/ui/SideBar.svelte
+++ src/lib/components/ui/SideBar.svelte
@@ -1,4 +1,11 @@
 <script lang="ts">
+    import Projects from "../Projects.svelte";
 </script>
 
-<div class=" w-[15%] h-[100%] border-r-2 "></div>
+<div class=" w-[15%] h-[100%] border-r-2 flex flex-col p-2 gap-2">
+    <button class=" bg-neutral-200 hover:bg-neutral-400 hover:text-white text-lg rounded-md p-3 flex items-center gap-2">
+        <Projects/>
+        <h1>Projects</h1>
+    </button>
+
+</div>
