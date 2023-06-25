06e231225f83a0dfe69ab1284f6b04d0d22abe58
TheWisePigeon <zozozozeph@gmail.com>
2023-06-25 17:27:07 +0000

    ui updates

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,17 +1,28 @@
-import type { Actions } from "@sveltejs/kit";
+import { redirect, type Actions } from "@sveltejs/kit";
 import sql from "$lib/db"
+import bcrypt from "bcrypt"
+import { loading } from "./store"
 
 export const actions = {
     default: async ({ request })=>{
         const data = await request.formData()
-        const email = data.get("email")!
-        const username = data.get("username")!
-        const password = data.get("password")!
-        console.log(email, username, password)
-        return {
-            error: "nothing"
+        const email = data.get("email")! as string
+        const username = data.get("username")! as string
+        const password = data.get("password")! as string
+        const { rowCount } = await sql` select email from users where email=${email} `
+        if(rowCount!==0){
+            loading.set(false)
+            return {
+                error: "Email already in use",
+                email, username, password
+            }
         }
-
-
+        const hashed_pwd = await bcrypt.hash(password, 10)
+        const plan = "basic"
+        await sql`
+            INSERT INTO users(email, password, username, plan)
+            values(${email}, ${hashed_pwd}, ${username}, ${plan})
+        `
+        throw redirect(301, "/login")
     }    
 } satisfies Actions
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,48 +1,43 @@
 <script lang="ts">
-	import { goto } from "$app/navigation";
     import type { PageData, ActionData } from "./$types";
     export let form : ActionData
     import Loading from "$lib/components/Loading.svelte";
-	let email = '';
-    let username = '';
-	let password = '';
-    let loading = false
-    let error = ""
-    console.log(form)
+    import { loading } from "./store"
 </script>
 
 <div class="fixed flex h-screen w-screen items-center justify-center bg-white">
 	<div
 		class="max-h-[75vh] lg:max-h-[60vh] max-w-[90vw] rounded-lg p-4 transition-all md:max-w-[75vw] lg:max-w-[60vw] duration-500"
 	>
-		<form method="post" enctype="application/x-www-form-urlencoded" class=" flex flex-col gap-5">
+		<form method="post" class=" flex flex-col gap-5">
 			<input
-				bind:value={email}
-				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
+				class={`border-2 ${form?.error? "border-red-500" :"border-gray-500 "} focus:outline-none p-2 rounded-md`}
 				type="email"
 				required
                 name="email"
+                value={form?.email ?? ""}
 				placeholder="Enter your email"
 			/>
 			<input
-				bind:value={username}
 				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
 				type="text"
 				required
                 name="username"
+                value={form?.username ?? ""}
 				placeholder="Enter your username"
 			/>
 			<input
-				bind:value={password}
 				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
 				type="password"
 				required
                 name="password"
 				placeholder="Enter your password"
 			/>
-            <h1 class=" text-xs text-red-500 text-center">{error}</h1>
-			<button disabled={loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
-                {#if !loading}
+            {#if form?.error}
+               <small class="text-center text-red-700 ">Email already in use</small> 
+            {/if}
+			<button on:click={()=>{ loading.set(true) }} disabled={$loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
+                {#if !$loading}
                     <h1>Register</h1>
                 {:else}
                     <Loading/>
