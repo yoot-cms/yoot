41207d446b2ffef8323d980783144457c1c3feea
TheWisePigeon <zozozozeph@gmail.com>
2023-06-25 09:11:18 +0000

    add stores and dynamic location change

---
+++ src/routes/(console)/console/projects/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { location } from "$lib/stores";
+    location.set("/console/projects")
+</script>
+
+<div class=" w-full h-[100%]  border-2 border-dashed flex flex-col">
+    <div class="h-[5%] border-b-2 flex items-center justify-between">
+        <h1>Projects</h1>
+    </div>
+    <div>
+        <h1>Content</h1>
+    </div> 
+</div>
