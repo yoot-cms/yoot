ebab7f19402a540c4b53e73493334bccbf69d488
TheWisePigeon <zozozozeph@gmail.com>
2023-06-25 08:29:48 +0000

    ui: updates

--- src/routes/(console)/+layout.svelte
+++ src/routes/(console)/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
     import SideBar from "$lib/components/ui/SideBar.svelte";
-    import Avatar from "$lib/components/Avatar.svelte";
 </script>
 
 <div class=" h-screen w-full">
@@ -10,7 +9,7 @@
         <div class=" flex gap-5 items-center">
             <a href="/pricing" class="underline">Pricing</a>
             <a href="/docs" class="underline">Documentation</a>
-            <a href="/docs" class="underline">Quick links</a>
+            <button class="underline">Quick links</button>
         </div>
     </div>
     <div class=" flex w-full h-[92%] ">
