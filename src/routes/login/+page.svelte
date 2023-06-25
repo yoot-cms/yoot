5f12fa4de03607bcb61923645a7fd5ad57b97c09
TheWisePigeon <zozozozeph@gmail.com>
2023-06-25 21:36:24 +0000

    updates

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,59 +1,36 @@
 <script lang="ts">
-	import { browser } from "$app/environment";
-	import { goto } from "$app/navigation";
     import Loading from "$lib/components/Loading.svelte";
-    import { API_URL } from "$lib/utils";
-	let email = '';
-	let password = '';
-    let loading = false
-    let error = ""
-	async function login() {
-        loading = true
-        error = ""
-		try {
-			const response = await fetch(`${API_URL}/auth/login`, {
-				method: 'POST',
-				body: JSON.stringify({ email, password })
-			});
-            loading = false
-            if (response.status===500) {
-                error = "Something went wrong. Please try again or contact us"
-            }
-            if (response.status===400) {
-                error = "Invalid credentials"
-            }
-            if (response.status===200) {
-                const { session_id } = await response.json() as { session_id : string }
-                goto("/dashboard")
-            }
-		} catch (err) {
-			console.log(err);
-		}
-	}
+    import type { ActionData } from "./$types";
+    export let form: ActionData
+    import { loading } from "./store";
 </script>
 
 <div class="fixed flex h-screen w-screen items-center justify-center bg-white">
 	<div
 		class="max-h-[75vh] lg:max-h-[60vh] max-w-[90vw] rounded-lg p-4 transition-all md:max-w-[75vw] lg:max-w-[60vw] duration-500"
 	>
-		<form on:submit|preventDefault={login} class=" flex flex-col gap-5">
+		<form method="post" class=" flex flex-col gap-2">
 			<input
-				bind:value={email}
+				value={form?.email ?? ""}
 				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
 				type="email"
+                name="email"
 				required
 				placeholder="Enter your email"
 			/>
 			<input
-				bind:value={password}
+				value={form?.password ?? ""}
 				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
 				type="password"
+                name="password"
 				required
 				placeholder="Enter your password"
 			/>
-            <h1 class=" text-xs text-red-500 text-center">{error}</h1>
-			<button disabled={loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
-                {#if !loading}
+            {#if form?.error}
+                <small class=" text-center text-red-500 ">{form.error}</small>
+            {/if}
+			<button on:click={()=>{ loading.set(true) }} disabled={$loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
+                {#if !$loading}
                     <h1>Login</h1>
                 {:else}
                     <Loading/>
