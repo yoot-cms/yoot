c4d8a9bf4a652a246884f2c6a673c939bde26dbb
TheWisePigeon <zozozozeph@gmail.com>
2023-06-26 17:11:54 +0000

    ui updates

---
+++ src/lib/components/ui/Project.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    export let name : string
+    export let id: string
+</script>
+
+<button class=" flex justify-center text-center items-center w-52 h-52 rounded-md border-2 p-2 ">
+    <h1>{name}</h1>
+</button>
---
+++ src/routes/(console)/console/projects/+page.server.ts
@@ -0,0 +1,18 @@
+import { redirect } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import { sql } from "@vercel/postgres";
+export const ssr = false
+
+export const load: PageServerLoad = async ({ locals }) => {
+    const { user_id } = locals
+    if(!user_id){
+        redirect(301, "/login")
+    }
+    console.log("loading rows")
+    const { rows } = await sql<{ id:string, name:string }>` select id, name from project where owner=${user_id} `
+    console.log("returning rows")
+    return {
+        data: rows
+    }
+
+} 
