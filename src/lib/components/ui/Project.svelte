c406fc8ac50ccf59949b19ee331103cde9ea2dc8
TheWisePigeon <zozozozeph@gmail.com>
2023-06-27 08:06:33 +0000

    ui: upadted svg icon in Project component

--- src/lib/components/ui/Project.svelte
+++ src/lib/components/ui/Project.svelte
@@ -4,7 +4,7 @@
     import Options from "../Options.svelte";
 </script>
 
-<button class="relative flex justify-center text-center items-center w-72 h-52 rounded-xl border-2 p-2 ">
+<button class="hover:border-neutral-500 relative flex justify-center text-center items-center w-72 h-52 rounded-xl border-2 p-2 ">
     <Options />
     <h1>{name}</h1>
 </button>
