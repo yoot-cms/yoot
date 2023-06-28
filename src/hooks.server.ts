747dfb5d70a2266b4ef8d18620d06580d98fdab8
TheWisePigeon <zozozozeph@gmail.com>
2023-06-28 23:47:11 +0000

    minor ui updates

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,12 @@
 import type { Handle } from "@sveltejs/kit";
 import kv from "$lib/kv"
 
-export const handle : Handle = async ({ event, resolve })=>{
-    const { cookies } = event
-    const session_id = cookies.get("session") ?? ""
-    const user_id = await kv.get(session_id)
-    if(typeof user_id === "string"){
-        event.locals.user_id = user_id
-    }
-    return await resolve(event)
-}
+// export const handle : Handle = async ({ event, resolve })=>{
+//     const { cookies } = event
+//     const session_id = cookies.get("session") ?? ""
+//     const user_id = await kv.get(session_id)
+//     if(typeof user_id === "string"){
+//         event.locals.user_id = user_id
+//     }
+//     return await resolve(event)
+// }
---
+++ src/routes/(board)/+layout.svelte
@@ -0,0 +1,15 @@
+<div class=" h-screen w-full ">
+    <div class=" w-full h-[4%] flex justify-center bg-slate-200 p-1 ">
+        <h1 class="text-center">Yoot is currently in Alpha</h1>
+    </div>
+    <div class=" flex w-full h-[96%]">
+        <div class="flex flex-col justify-between items-center border-r w-[10%] bg-slate-100 ">
+            <div class=" h-[10%] w-full border-b flex flex-col items-center justify-center">
+                <h1 class=" font-bold ">YOOT</h1>
+            </div>
+        </div>
+        <div class=" w-full flex flex-col ">
+            <div class=" border-b p-5 h-[10%] w-full"></div>
+        </div>
+    </div>
+</div>
