227daf77e56ccd941039fd73d3fc28cbb6a3c57b
TheWisePigeon <zozozozeph@gmail.com>
2023-06-29 21:00:26 +0000

    ui updates

--- src/routes/(console)/+layout.svelte
+++ src/routes/(console)/+layout.svelte
@@ -5,8 +5,8 @@
 
 <div class=" w-full h-screen flex ">
     <SideBar />
-    <div class=" flex flex-col w-full h-full bg-neutral-100">
-        <div class=" flex justify-between items-center p-5 w-full h-20 border-b border-neutral-300 ">
+    <div class=" flex flex-col w-full h-full ">
+        <div class=" flex justify-between items-center p-5 w-full h-20 border-b ">
             {#each $breadcrumb_items as breadcrumb_item}
                <a href={breadcrumb_item.path} class=" hover:underline ">{breadcrumb_item.title}</a> 
             {/each}
