43fb74573a9a900b733b5d8290b51a8739f6138c
TheWisePigeon <zozozozeph@gmail.com>
2023-07-01 15:05:17 +0000

    fix: error handling

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -6,34 +6,29 @@ import { loading } from "./store";
 
 export const actions: Actions = {
     default: async ({ request, cookies }) => {
-        try {
-            const data = await request.formData()
-            const email = data.get("email")! as string
-            const password = data.get("password")! as string
-            const { rows, rowCount } = await sql<{ email: string, password: string, id: string, plan: string }>` select * from users where email=${email} `
-            if (rowCount === 0) {
-                loading.set(false)
-                return fail(400)
-            }
-            const [user] = rows
-            const password_is_valid = await bcrypt.compare(password, user.password)
-            if (!password_is_valid) {
-                loading.set(false)
-                return fail(400)
-            }
-            const session_id = crypto.randomUUID()
-            await kv.set(session_id, user.id)
-            cookies.set("session", session_id, {
-                path: "/",
-                httpOnly: true,
-                sameSite: 'strict',
-                maxAge: 60 * 60 * 24 * 30,
-                secure: process.env.NODE_ENV === "production"
-            })
-            throw redirect(302, '/console')
-        } catch (error) {
-            console.log(error)
-            return fail(500)
+        const data = await request.formData()
+        const email = data.get("email")! as string
+        const password = data.get("password")! as string
+        const { rows, rowCount } = await sql<{ email: string, password: string, id: string, plan: string }>` select * from users where email=${email} `
+        if (rowCount === 0) {
+            loading.set(false)
+            return fail(400)
         }
+        const [user] = rows
+        const password_is_valid = await bcrypt.compare(password, user.password)
+        if (!password_is_valid) {
+            loading.set(false)
+            return fail(400)
+        }
+        const session_id = crypto.randomUUID()
+        await kv.set(session_id, user.id)
+        cookies.set("session", session_id, {
+            path: "/",
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 24 * 30,
+            secure: process.env.NODE_ENV === "production"
+        })
+        throw redirect(302, '/console')
     }
 }
