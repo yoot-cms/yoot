854b91f99c2ea3bb9b03a7e512a090d2abe5d772
TheWisePigeon <zozozozeph@gmail.com>
2023-07-02 18:31:28 +0000

    ui: added shared tab and keys page

---
+++ src/routes/(console)/console/keys/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { location, breadcrumb_items } from "$lib/stores";
+    location.set('/console/keys')
+    breadcrumb_items.set([
+    { title:"Keys", path:"/console/keys" }
+    ])
+</script>
+
+<h1>Sup</h1>
