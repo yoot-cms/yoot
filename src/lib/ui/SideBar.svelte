a4dc7e5ec1905876bc3ae0521f9c3fa0d44568c0
TheWisePigeon <zozozozeph@gmail.com>
2023-07-02 18:31:08 +0000

    minor updates

--- src/lib/ui/SideBar.svelte
+++ src/lib/ui/SideBar.svelte
@@ -5,6 +5,7 @@
 	import ApiKeys from '$lib/components/ApiKeys.svelte';
 	import Avatar from '$lib/components/Avatar.svelte';
 	import Stats from '$lib/components/Stats.svelte';
+	import Shared from '$lib/components/Shared.svelte';
 	const items: { name: string; path: string; icon: typeof SvelteComponent }[] = [
 		{
 			name: 'Projects',
@@ -17,17 +18,26 @@
 			icon: ApiKeys
 		},
 		{
+			name: 'Shared',
+			path: '/console/shared',
+			icon: Shared
+		},
+		{
 			name: 'Stats',
 			path: '/console/stats',
 			icon: Stats
 		}
 	];
-    export let user_data : { name:string, profile_pic:string }
+	export let user_data: { name: string; profile_pic: string };
 </script>
 
-<div class=" w-64 p-5 flex flex-col justify-between h-full border-r border-neutral-300 ">
+<div class=" w-64 p-5 flex flex-col justify-between h-full border-r border-neutral-300">
 	<div class="flex flex-col gap-5">
-        <a href="/console/" class=" font-bold bg-clip-text mb-1 text-transparent text-2xl text-center bg-gradient-to-t from-rose-700 to-pink-600">YOOT</a>
+		<a
+			href="/console/"
+			class=" font-bold bg-clip-text mb-1 text-transparent text-2xl text-center bg-gradient-to-t from-rose-700 to-pink-600"
+			>YOOT</a
+		>
 		{#each items as item}
 			<a
 				href={item.path}
@@ -40,13 +50,15 @@
 			</a>
 		{/each}
 	</div>
-			<a
-				href="/profile"
-				class={` ${
-					$location === "/console/profile" ? 'bg-neutral-600 text-white hover:text-black' : ' text-black'
-				}  hover:bg-neutral-100  rounded-md p-3 flex items-center gap-2 transition-all`}
-			>
-				<svelte:component this={Avatar} />
-				<h1>{user_data.name}</h1>
-			</a>
+	<a
+		href="/profile"
+		class={` ${
+			$location === '/console/profile'
+				? 'bg-neutral-600 text-white hover:text-black'
+				: ' text-black'
+		}  hover:bg-neutral-100  rounded-md p-3 flex items-center gap-2 transition-all`}
+	>
+		<svelte:component this={Avatar} />
+		<h1>{user_data.name}</h1>
+	</a>
 </div>
