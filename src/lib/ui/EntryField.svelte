40c0da363ab321f82f5e98047ef4e3ca6e2bee3c
TheWisePigeon <zozozozeph@gmail.com>
2023-07-09 21:02:30 +0000

    ui: added Entryfield component

---
+++ src/lib/ui/EntryField.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let data_type: string;
+	export let field_name: string;
+</script>
+
+{#if data_type === 'Text'}
+	<input class=" border p-2 rounded-md w-full focus:outline-none" placeholder={field_name} type="text" name={field_name} />
+{/if}
+
+{#if data_type === 'Number'}
+	<input class=" border p-2 rounded-md w-full focus:outline-none" placeholder={field_name} type="number" name={field_name} />
+{/if}
+
+{#if data_type === 'Boolean'}
+	<input  class=" border p-2 rounded-md w-full focus:outline-none" placeholder={field_name} type="checkbox" name={field_name} />
+{/if}
+
+{#if data_type === 'Image'}
+	<input class=" border p-2 rounded-md w-full focus:outline-none" placeholder={field_name} type="file" accept="image/*" name={field_name} />
+{/if}
