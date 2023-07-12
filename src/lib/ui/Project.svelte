b53c326e596d198e5b6b68f8860e722b473638e6
TheWisePigeon <zozozozeph@gmail.com>
2023-07-12 09:29:13 +0000

    fix: truncate project name

--- src/lib/ui/Project.svelte
+++ src/lib/ui/Project.svelte
@@ -7,7 +7,7 @@
 	on:click={() => {
 		goto(`/console/projects/${name}`);
 	}}
-	class="p-2 drop-shadow-lg hover:drop-shadow-xl relative group h-[200px] w-[200px] bg-neutral-50 border-neutral-200 rounded-lg transition-all"
+	class="truncate p-2 drop-shadow-lg hover:drop-shadow-xl relative group h-[200px] w-[200px] bg-neutral-50 border-neutral-200 rounded-lg transition-all"
 >
-	<h1 class=" group-hover:text-neutral-900 text-2xl text-neutral-700 font-bold">{name}</h1>
+	<h1 class="truncate group-hover:text-neutral-900 text-2xl text-neutral-700 font-bold">{name}</h1>
 </button>
