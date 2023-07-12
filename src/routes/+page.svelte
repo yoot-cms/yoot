cb1287302bde22851cb34793b6ab660ca2a5819f
TheWisePigeon <zozozozeph@gmail.com>
2023-07-12 09:27:57 +0000

    redirect user to login page if app in prod mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	onMount(() => {
+		if (import.meta.env.PROD) {
+			goto('/login');
+		}
+	});
 	import Entry from '$lib/ui/Entry.svelte';
+	import { onMount } from 'svelte';
 	const schema = {
 		name: 'Text',
 		age: 'Number',
