02476f8ed9685d0566fc2734666725b2e7b757e9
TheWisePigeon <zozozozeph@gmail.com>
2023-07-14 17:43:44 +0000

    started building landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,3 @@
-<script lang="ts">
-	import { goto } from '$app/navigation';
-	onMount(() => {
-		if (import.meta.env.PROD) {
-			goto('/login');
-		}
-	});
-	import Entry from '$lib/ui/Entry.svelte';
-	import { onMount } from 'svelte';
-	const schema = {
-		name: 'Text',
-		age: 'Number',
-		profile: 'Image',
-		alive: 'Boolean'
-	};
-	const entry = {
-		id: 'duhhuh',
-		entity: 'duh',
-		value: {
-			name: 'Joseph',
-			age: 2,
-			profile: 'https://picsum.photos/200/300',
-			alive: true
-		}
-	};
-</script>
-
-<div
-	class="pl-96 w-screen flex gap-7 h-screen pt-32 p-8 overflow-y-auto overflow-x-hidden max-h-screen items-start flex-wrap justify-start"
->
-	<Entry {schema} {entry} />
-</div>
+<header>
+    <h1>Welcome to YOOT</h1>
+</header>
