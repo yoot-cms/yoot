73dfe8bb66fca101ecedfb77e7a0f30270fac52d
TheWisePigeon <zozozozeph@gmail.com>
2023-08-02 00:03:11 +0000

    minor updates

--- src/routes/(console)/+layout.svelte
+++ src/routes/(console)/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import SideBar from '$lib/ui/SideBar.svelte';
 	import { breadcrumb_items } from '$lib/stores';
-    import type { LayoutData } from './$types';
-    export let data : LayoutData
-    $: ({ user } = data)
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
+	$: ({ user } = data);
 </script>
 
-<div class="  w-full h-screen flex">
+<div class=" w-full h-screen flex">
 	<SideBar user_data={{ name: user.username, profile_pic: user.profile_pic }} />
 	<div class=" flex flex-col w-full h-full">
 		<div class=" flex justify-between items-center p-5 w-full h-20 border-b">
@@ -16,10 +16,10 @@
 				{/each}
 			</div>
 			<div class=" flex gap-5">
-				<h1>{` ${user.plan==="basic"?"Free":"Pro"} `}</h1>
-                {#if user.plan==="basic"}
-                    <h1>Upgrade to pro</h1>
-                {/if}
+				<h1>{` ${user.plan === 'basic' ? 'Free' : 'Pro'} `}</h1>
+				{#if user.plan === 'basic'}
+					<h1>Upgrade to pro</h1>
+				{/if}
 			</div>
 		</div>
 		<slot />
