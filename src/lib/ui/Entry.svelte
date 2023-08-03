10b10b0e4c9b5c10d351bffcc0f2e50add77ea13
TheWisePigeon <zozozozeph@gmail.com>
2023-08-03 15:52:50 +0000

    dev: boolean field preview

--- src/lib/ui/Entry.svelte
+++ src/lib/ui/Entry.svelte
@@ -3,7 +3,7 @@
 	export let entry: {
 		id: string;
 		entity: string;
-		value: Record<string, string | number | boolean>;
+		value: Record<string, string | number | boolean | any>;
 	};
 </script>
 
@@ -17,8 +17,19 @@
 					alt=""
 				/>
 			</div>
+		{:else if field_type === 'Boolean'}
+			<div class=" flex flex-col gap-2">
+				<label class="relative inline-flex items-center cursor-pointer">
+					<input type="checkbox" checked={entry.value[field_name]} class="sr-only peer" />
+					<div
+						class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
+					/>
+				</label>
+			</div>
 		{:else}
-			<h1 title={entry.value[field_name].toString()} class=" truncate w-[300px] shrink-0">{entry.value[field_name]}</h1>
+			<h1 title={entry.value[field_name].toString()} class=" truncate w-[300px] shrink-0">
+				{entry.value[field_name]}
+			</h1>
 		{/if}
 	{/each}
 </div>
