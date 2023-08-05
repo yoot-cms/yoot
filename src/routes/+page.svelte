91ac200ae17e24c864869d90da786b15bde7c7f9
TheWisePigeon <zozozozeph@gmail.com>
2023-08-05 06:29:50 +0000

    ui: updates to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -140,7 +140,7 @@
 					>
 				</div>
 				<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-					<a href="#" class="text-sm font-semibold leading-6 text-white"
+					<a href="/login" class="text-sm font-semibold leading-6 text-white"
 						>Log in <span aria-hidden="true">&rarr;</span></a
 					>
 				</div>
@@ -315,16 +315,14 @@
 							</p>
 							<div class="mt-10 flex items-center justify-center gap-x-6">
 								<a
-									href="/console"
-									class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
-									>Get Started</a
-								>
-								<a
 									href="https://docs.yootcms.xyz"
 									rel="noreferrer"
 									target="_blank"
-									class="text-sm font-semibold leading-5 text-white"
-									>Documentation<span aria-hidden="true">→</span></a
+									class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
+									>Documentation</a
+								>
+								<a href="/console" class="text-sm font-semibold leading-5 text-white"
+									>Get Started<span aria-hidden="true">→</span></a
 								>
 							</div>
 						</div>
@@ -352,11 +350,12 @@
 					<div class="mx-auto max-w-2xl sm:text-center">
 						<h2 class="text-base font-semibold leading-7 text-blue-600">Everything you need</h2>
 						<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-							No server? No problem.
+							No server? No database? No problem.
 						</p>
 						<p class="mt-6 text-lg leading-8 text-gray-600">
-							YOOT offers a user friendly web dashboard that you can use to manage and store any
-							type of data.
+							YOOT offers a user friendly web dashboard that you can use to store and manage your
+							data, but also a simple to use REST API that exposes access to that data from anywhere
+							you can make an HTTP request from
 						</p>
 					</div>
 				</div>
@@ -381,182 +380,55 @@
 						<div class="relative pl-9">
 							<dt class="inline font-semibold text-gray-900">
 								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									class="absolute left-1 top-1 h-6 w-6 text-blue-600 fill-current"
+									viewBox="0 0 256 256"
+									><path
+										d="M216,40H40A16,16,0,0,0,24,56V200a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A16,16,0,0,0,216,40Zm0,16V88H40V56Zm0,144H40V104H216v96Z"
+									/></svg
 								>
-									<path
-										fill-rule="evenodd"
-										d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								Push to deploy.
+								Web console.
 							</dt>
 							<dd class="inline">
-								Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.
+								With YOOT console, manage your data on a user friendly interface
 							</dd>
 						</div>
 						<div class="relative pl-9">
 							<dt class="inline font-semibold text-gray-900">
 								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									class="absolute left-1 top-1 h-6 w-6 text-blue-600 fill-current"
+									viewBox="0 0 256 256"
+									><path
+										d="M160,16A80.07,80.07,0,0,0,83.91,120.78L26.34,178.34A8,8,0,0,0,24,184v40a8,8,0,0,0,8,8H72a8,8,0,0,0,8-8V208H96a8,8,0,0,0,8-8V184h16a8,8,0,0,0,5.66-2.34l9.56-9.57A80,80,0,1,0,160,16Zm0,144a63.7,63.7,0,0,1-23.65-4.51,8,8,0,0,0-8.84,1.68L116.69,168H96a8,8,0,0,0-8,8v16H72a8,8,0,0,0-8,8v16H40V187.31l58.83-58.82a8,8,0,0,0,1.68-8.84A64,64,0,1,1,160,160Zm32-84a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"
+									/></svg
 								>
-									<path
-										fill-rule="evenodd"
-										d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								SSL certificates.
+
+								API keys.
 							</dt>
 							<dd class="inline">
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-								commodo.
+								Create secure keys with specific permissions on your projects and share these keys
+								with developers you work with
 							</dd>
 						</div>
 						<div class="relative pl-9">
 							<dt class="inline font-semibold text-gray-900">
 								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									class="absolute left-1 top-1 h-6 w-6 text-blue-600 fill-current"
+									viewBox="0 0 256 256"
+									><path
+										d="M117.66,170.34a8,8,0,0,1,0,11.32l-32,32a8,8,0,0,1-11.32,0l-32-32a8,8,0,0,1,11.32-11.32L72,188.69V48a8,8,0,0,1,16,0V188.69l18.34-18.35A8,8,0,0,1,117.66,170.34Zm96-96-32-32a8,8,0,0,0-11.32,0l-32,32a8,8,0,0,0,11.32,11.32L168,67.31V208a8,8,0,0,0,16,0V67.31l18.34,18.35a8,8,0,0,0,11.32-11.32Z"
+									/></svg
 								>
-									<path
-										fill-rule="evenodd"
-										d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								Simple queues.
-							</dt>
-							<dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.</dd>
-						</div>
-						<div class="relative pl-9">
-							<dt class="inline font-semibold text-gray-900">
-								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M10 2.5c-1.31 0-2.526.386-3.546 1.051a.75.75 0 01-.82-1.256A8 8 0 0118 9a22.47 22.47 0 01-1.228 7.351.75.75 0 11-1.417-.49A20.97 20.97 0 0016.5 9 6.5 6.5 0 0010 2.5zM4.333 4.416a.75.75 0 01.218 1.038A6.466 6.466 0 003.5 9a7.966 7.966 0 01-1.293 4.362.75.75 0 01-1.257-.819A6.466 6.466 0 002 9c0-1.61.476-3.11 1.295-4.365a.75.75 0 011.038-.219zM10 6.12a3 3 0 00-3.001 3.041 11.455 11.455 0 01-2.697 7.24.75.75 0 01-1.148-.965A9.957 9.957 0 005.5 9c0-.028.002-.055.004-.082a4.5 4.5 0 018.996.084V9.15l-.005.297a.75.75 0 11-1.5-.034c.003-.11.004-.219.005-.328a3 3 0 00-3-2.965zm0 2.13a.75.75 0 01.75.75c0 3.51-1.187 6.745-3.181 9.323a.75.75 0 11-1.186-.918A13.687 13.687 0 009.25 9a.75.75 0 01.75-.75zm3.529 3.698a.75.75 0 01.584.885 18.883 18.883 0 01-2.257 5.84.75.75 0 11-1.29-.764 17.386 17.386 0 002.078-5.377.75.75 0 01.885-.584z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								Advanced security.
+								Access your data.
 							</dt>
 							<dd class="inline">
-								Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.
+								Get access to your data from anywhere you can make an HTTP request from. Our simple
+								REST API let's you integrate YOOT seamlessly to all your apps
 							</dd>
-						</div>
-						<div class="relative pl-9">
-							<dt class="inline font-semibold text-gray-900">
-								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										fill-rule="evenodd"
-										d="M7.84 1.804A1 1 0 018.82 1h2.36a1 1 0 01.98.804l.331 1.652a6.993 6.993 0 011.929 1.115l1.598-.54a1 1 0 011.186.447l1.18 2.044a1 1 0 01-.205 1.251l-1.267 1.113a7.047 7.047 0 010 2.228l1.267 1.113a1 1 0 01.206 1.25l-1.18 2.045a1 1 0 01-1.187.447l-1.598-.54a6.993 6.993 0 01-1.929 1.115l-.33 1.652a1 1 0 01-.98.804H8.82a1 1 0 01-.98-.804l-.331-1.652a6.993 6.993 0 01-1.929-1.115l-1.598.54a1 1 0 01-1.186-.447l-1.18-2.044a1 1 0 01.205-1.251l1.267-1.114a7.05 7.05 0 010-2.227L1.821 7.773a1 1 0 01-.206-1.25l1.18-2.045a1 1 0 011.187-.447l1.598.54A6.993 6.993 0 017.51 3.456l.33-1.652zM10 13a3 3 0 100-6 3 3 0 000 6z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								Powerful API.
-							</dt>
-							<dd class="inline">
-								Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-								commodo.
-							</dd>
-						</div>
-						<div class="relative pl-9">
-							<dt class="inline font-semibold text-gray-900">
-								<svg
-									class="absolute left-1 top-1 h-5 w-5 text-blue-600"
-									viewBox="0 0 20 20"
-									fill="currentColor"
-									aria-hidden="true"
-								>
-									<path
-										d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z"
-									/>
-									<path
-										fill-rule="evenodd"
-										d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z"
-										clip-rule="evenodd"
-									/>
-								</svg>
-								Database backups.
-							</dt>
-							<dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.</dd>
 						</div>
 					</dl>
-				</div>
-			</div>
-
-			<!-- Testimonial section -->
-			<div class="relative z-10 mt-32 bg-gray-900 pb-20 sm:mt-56 sm:pb-24 xl:pb-0">
-				<div class="absolute inset-0 overflow-hidden" aria-hidden="true">
-					<div class="absolute left-[calc(50%-19rem)] top-[calc(50%-36rem)] transform-gpu blur-3xl">
-						<div
-							class="aspect-[1097/1023] w-[68.5625rem] bg-gradient-to-r from-[#ff4694] to-[#776fff] opacity-25"
-							style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-						/>
-					</div>
-				</div>
-				<div
-					class="mx-auto flex max-w-7xl flex-col items-center gap-x-8 gap-y-10 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch"
-				>
-					<div class="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
-						<div class="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
-							<img
-								class="absolute inset-0 h-full w-full rounded-2xl bg-gray-800 object-cover shadow-2xl"
-								src="/95161388.png"
-								alt=""
-							/>
-						</div>
-					</div>
-					<div class="w-full max-w-2xl xl:max-w-none xl:flex-auto xl:px-16 xl:py-24">
-						<figure class="relative isolate pt-6 sm:pt-12">
-							<svg
-								viewBox="0 0 162 128"
-								fill="none"
-								aria-hidden="true"
-								class="absolute left-0 top-0 -z-10 h-32 stroke-white/20"
-							>
-								<path
-									id="b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb"
-									d="M65.5697 118.507L65.8918 118.89C68.9503 116.314 71.367 113.253 73.1386 109.71C74.9162 106.155 75.8027 102.28 75.8027 98.0919C75.8027 94.237 75.16 90.6155 73.8708 87.2314C72.5851 83.8565 70.8137 80.9533 68.553 78.5292C66.4529 76.1079 63.9476 74.2482 61.0407 72.9536C58.2795 71.4949 55.276 70.767 52.0386 70.767C48.9935 70.767 46.4686 71.1668 44.4872 71.9924L44.4799 71.9955L44.4726 71.9988C42.7101 72.7999 41.1035 73.6831 39.6544 74.6492C38.2407 75.5916 36.8279 76.455 35.4159 77.2394L35.4047 77.2457L35.3938 77.2525C34.2318 77.9787 32.6713 78.3634 30.6736 78.3634C29.0405 78.3634 27.5131 77.2868 26.1274 74.8257C24.7483 72.2185 24.0519 69.2166 24.0519 65.8071C24.0519 60.0311 25.3782 54.4081 28.0373 48.9335C30.703 43.4454 34.3114 38.345 38.8667 33.6325C43.5812 28.761 49.0045 24.5159 55.1389 20.8979C60.1667 18.0071 65.4966 15.6179 71.1291 13.7305C73.8626 12.8145 75.8027 10.2968 75.8027 7.38572C75.8027 3.6497 72.6341 0.62247 68.8814 1.1527C61.1635 2.2432 53.7398 4.41426 46.6119 7.66522C37.5369 11.6459 29.5729 17.0612 22.7236 23.9105C16.0322 30.6019 10.618 38.4859 6.47981 47.558L6.47976 47.558L6.47682 47.5647C2.4901 56.6544 0.5 66.6148 0.5 77.4391C0.5 84.2996 1.61702 90.7679 3.85425 96.8404L3.8558 96.8445C6.08991 102.749 9.12394 108.02 12.959 112.654L12.959 112.654L12.9646 112.661C16.8027 117.138 21.2829 120.739 26.4034 123.459L26.4033 123.459L26.4144 123.465C31.5505 126.033 37.0873 127.316 43.0178 127.316C47.5035 127.316 51.6783 126.595 55.5376 125.148L55.5376 125.148L55.5477 125.144C59.5516 123.542 63.0052 121.456 65.9019 118.881L65.5697 118.507Z"
-								/>
-								<use href="#b56e9dab-6ccb-4d32-ad02-6b4bb5d9bbeb" x="86" />
-							</svg>
-							<blockquote
-								class="text-xl font-semibold leading-8 text-white sm:text-2xl sm:leading-9"
-							>
-								<p>
-									For me YOOT takes away the pain I feel every time I have to build a simple CRUD
-									system. I just slap the data in there and use the API to access it.
-								</p>
-							</blockquote>
-							<figcaption class="mt-8 text-base">
-								<div class="font-semibold text-white">
-									<a href="https://github.com/TheWisePigeon" rel="noreferrer" target="_blank"
-										>TheWisePigeon</a
-									>
-								</div>
-								<div class="mt-1 text-gray-400">Yoot creator</div>
-							</figcaption>
-						</figure>
-					</div>
 				</div>
 			</div>
 
@@ -574,12 +446,12 @@
 				<div class="mx-auto max-w-2xl text-center lg:max-w-4xl">
 					<h2 class="text-base font-semibold leading-7 text-blue-600">Pricing</h2>
 					<p class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-						YOOT is free to use for now, with some limitations.
+						YOOT is free to use with some limitations.
 					</p>
 				</div>
 				<p class="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-600">
 					We just launched, and paid plans are the last thing we are thinking about right now. Feel
-					free to use YOOT as you like. We will be updating this section asap
+					free to use YOOT as you like.
 				</p>
 				<div
 					class="mx-auto mt-16 grid max-w-lg grid-cols-1 items-center gap-y-6 sm:mt-20 sm:gap-y-0 lg:max-w-4xl lg:grid-cols-2"
@@ -593,9 +465,9 @@
 							<span class="text-base text-gray-500">/month</span>
 						</p>
 						<p class="mt-6 text-base leading-7 text-gray-600">
-							The perfect plan if you&#039;re just getting started with our product.
+							Get started with Yoot today and tell us what you think
 						</p>
-						<ul role="list" class="mt-8 space-y-3 text-sm leading-6 sm:mt-10 text-gray-600">
+						<ul class="mt-8 space-y-3 text-sm leading-6 sm:mt-10 text-gray-600">
 							<li class="flex gap-x-3">
 								<svg
 									class="h-6 w-5 flex-none text-blue-600"
@@ -890,18 +762,19 @@
 									<h3 class="text-sm font-semibold leading-6 text-white">Developers</h3>
 									<ul class="mt-6 space-y-4">
 										<li>
-											<a href="https://github.com/TheWisePigeon/yoot" target="_blank" rel="noreferrer" class="text-sm leading-6 text-gray-300 hover:text-white"
-												>GitHub</a
+											<a
+												href="https://github.com/TheWisePigeon/yoot"
+												target="_blank"
+												rel="noreferrer"
+												class="text-sm leading-6 text-gray-300 hover:text-white">GitHub</a
 											>
 										</li>
 										<li>
-											<a href="https://docs.yootcms.xyz" target="_blank" rel="noreferrer" class="text-sm leading-6 text-gray-300 hover:text-white"
-												>Documentation</a
-											>
-										</li>
-										<li>
-											<a href="https://github.com/dev-mantas" target="_blank" rel="noreferrer" class="text-sm leading-6 text-gray-300 hover:text-white"
-												>Credits to @MantasDev for this landing page</a
+											<a
+												href="https://docs.yootcms.xyz"
+												target="_blank"
+												rel="noreferrer"
+												class="text-sm leading-6 text-gray-300 hover:text-white">Documentation</a
 											>
 										</li>
 									</ul>
@@ -910,8 +783,9 @@
 									<h3 class="text-sm font-semibold leading-6 text-white">Support</h3>
 									<ul class="mt-6 space-y-4">
 										<li>
-											<a href="mailto:yootcms@gmail.com" class="text-sm leading-6 text-gray-300 hover:text-white"
-												>Email us</a
+											<a
+												href="mailto:yootcms@gmail.com"
+												class="text-sm leading-6 text-gray-300 hover:text-white">Email us</a
 											>
 										</li>
 									</ul>
