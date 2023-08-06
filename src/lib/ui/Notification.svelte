c4bf0ce15259c67330bc3adc421d5f7b532b59fb
TheWisePigeon <zozozozeph@gmail.com>
2023-08-06 07:59:14 +0000

    ui: new icon components and updates on notification components

--- src/lib/ui/Notification.svelte
+++ src/lib/ui/Notification.svelte
@@ -1,5 +1,24 @@
 <script lang="ts">
+  import Check from "$lib/components/Check.svelte";
+  import OpenInvitation from "$lib/components/OpenInvitation.svelte";
+  export let notification: { id:string, type:"notification"|"invitation", read:boolean, invitation:string, notifiee:string }
+  export let show_read: boolean
 </script>
 
-<div>
+<div hidden={show_read&&notification.read} class={` w-full ${notification.read?"bg-white":"bg-neutral-300"} rounded-md p-2 flex justify-between `}>
+  <h1>Some message</h1>
+  <div class=" flex gap-2">
+    {#if notification.type==="invitation"}
+      <button>
+        <OpenInvitation/>
+      </button>
+    {:else}
+      <button>
+        <Check/>
+      </button>
+    {/if}
+    <button class=" ">
+      <Check/>
+    </button>
+  </div>
 </div>
--- src/lib/ui/modals/NotificationsBoard.svelte
+++ src/lib/ui/modals/NotificationsBoard.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-  export let notifications: { id:string, type:"notification"|"invitation", read:boolean, invitation:string }[]
+	export let notifications: {
+		id: string;
+		type: 'notification' | 'invitation';
+		read: boolean;
+		invitation: string;
+		notifiee: string;
+	}[];
 	import { show_notifications } from '$lib/stores';
-  import Close from '$lib/components/Close.svelte';
-  import Loading from '$lib/components/Loading.svelte';
-  import Notification from '../Notification.svelte';
-  let loading  = false
+	import Close from '$lib/components/Close.svelte';
+	import Notification from '../Notification.svelte';
+	let loading = false;
+	$: show_read = false
 </script>
 
 {#if $show_notifications}
@@ -13,17 +19,28 @@
 	>
 		<div class=" p-5 bg-white w-[50rem] rounded-lg flex flex-col gap-5">
 			<div class=" flex justify-between items-center">
-        <h1>Notifications</h1>
-				<button
-					on:click={() => {
-						if (!loading) {
-							show_notifications.set(false);
-						}
-					}}
-				>
-					<Close />
-				</button>
+				<h1>Notifications</h1>
+				<div class=" flex gap-2">
+					<label title="Show read notifications" class="relative inline-flex items-center cursor-pointer">
+						<input type="checkbox" checked={show_read} on:change={()=>{ show_read = !show_read }} class="sr-only peer" />
+						<div
+							class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
+						/>
+					</label>
+					<button
+						on:click={() => {
+							if (!loading) {
+								show_notifications.set(false);
+							}
+						}}
+					>
+						<Close />
+					</button>
+				</div>
 			</div>
+			{#each notifications as notification}
+				<Notification {notification} {show_read} />
+			{/each}
 		</div>
 	</div>
 {/if}
