e27125434caedfa3783de5dc49b0ea928a4e1378
TheWisePigeon <zozozozeph@gmail.com>
2023-08-21 08:59:42 +0000

    ui: registration page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -13,51 +13,91 @@
 					return;
 			}
 			toast.success('Account created!');
-			window.location.href="/console"
+			window.location.href = '/login';
 			await update();
 		};
 	};
 </script>
 
-<div class="fixed flex h-screen w-screen items-center justify-center bg-white">
-	<div
-		class="max-h-[75vh] lg:max-h-[60vh] max-w-[90vw] rounded-lg p-4 transition-all md:max-w-[75vw] lg:max-w-[60vw] duration-500"
-	>
-		<form use:enhance={handle_registration} method="post" class=" flex flex-col gap-2">
-			<input
-				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
-				type="email"
-				required
-				autocomplete="off"
-				name="email"
-				placeholder="Enter your email"
-			/>
-			<input
-				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
-				type="text"
-				autocomplete="off"
-				required
-				name="username"
-				placeholder="Enter your username"
-			/>
-			<input
-				class="border-2 border-gray-500 focus:outline-none p-2 rounded-md"
-				type="password"
-				autocomplete="off"
-				required
-				name="password"
-				placeholder="Enter your password"
-			/>
-			<button disabled={loading} class="flex justify-center p-2 bg-blue-500 text-white rounded-md">
-				{#if !loading}
-					<h1>Register</h1>
-				{:else}
-					<Loading />
-				{/if}
-			</button>
-			<a class="text-sm text-blue-500 underline" href="/login"
-				>Already have an account? Login instead</a
-			>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+	<div class="sm:mx-auto sm:w-full sm:max-w-sm">
+		<img
+			class="mx-auto h-10 w-auto"
+			src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+			alt="Your Company"
+		/>
+		<h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+			Welcome to YOOT
+		</h2>
+	</div>
+
+	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+		<form use:enhance={handle_registration} class="space-y-6" method="POST">
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6 text-gray-900"
+					>Email address</label
+				>
+				<div class="mt-2">
+					<input
+						name="email"
+						type="email"
+						autocomplete="off"
+						required
+						class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Username</label
+				>
+				<div class="mt-2">
+					<input
+						name="username"
+						type="text"
+						autocomplete="off"
+						required
+						class="block p-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+			<div>
+				<div class="flex items-center justify-between">
+					<label for="password" class="block text-sm font-medium leading-6 text-gray-900"
+						>Password</label
+					>
+				</div>
+				<div class="mt-2">
+					<input
+						name="password"
+						type="password"
+						autocomplete="off"
+						required
+						class="block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<button
+          disabled={loading}
+					type="submit"
+					class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+          {#if !loading}
+            <h1>Register</h1>
+          {:else}
+            <Loading/>
+          {/if}
+          </button
+				>
+			</div>
 		</form>
+
+		<p class="mt-10 text-center text-sm text-gray-500">
+			Already have an account?
+			<a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+				>Login instead</a
+			>
+		</p>
 	</div>
 </div>
