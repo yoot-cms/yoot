9e2f2ebf2bcd43d60a90c6a9906a608be20c06aa
TheWisePigeon <zozozozeph@gmail.com>
2023-08-21 16:44:48 +0000

    display info about current entries

--- src/routes/(console)/console/shares/[share]/[entity]/+page.server.ts
+++ src/routes/(console)/console/shares/[share]/[entity]/+page.server.ts
@@ -1,22 +1,40 @@
 import type { PageServerLoad } from "./$types";
 import sql from "$lib/db";
 import { redirect } from "@sveltejs/kit";
+type Entity = {
+  id: string,
+  name: string,
+  schema: Record<string, string>,
+  trashed: boolean
+}
+
+type Entry = {
+  id: string,
+  entity: string,
+  value: Record<string, string>
+}
 
 export const load: PageServerLoad = async ({ locals: { user }, params }) => {
   const { share, entity } = params
-  const [targetted_share]: { sharee: string, project: string }[] = await sql` select sharee, project from shares where id=${share}`
-  if (!targetted_share) {
-    return redirect(301, '/console/shares')
-  }
-  if (user.id !== targetted_share.sharee) {
-    return redirect(301, '/console/shares')
-  }
-  const [targetted_entity]: { id: string }[] = await sql`select id from entity where name=${entity} and project=${targetted_share.project}`
-  if(!targetted_entity){
-    return redirect(301, '/console/shares')
-  }
-  const entries = await sql`select * from entry where entity=${targetted_entity.id} `
+  const [targetted_share]: { sharee: string, project: string, sharer: string, project_name:string }[] = await sql` 
+    SELECT shares.sharee, shares.project, shares.sharer, users.email AS sharer, project.name AS project_name
+    FROM shares
+    JOIN users ON shares.sharer = users.id
+    JOIN project ON shares.project = project.id
+    WHERE shares.id = ${share}
+  `
+  console.log(targetted_share)
+  if (!targetted_share) throw redirect(301, '/console/shares')
+  if (user.id !== targetted_share.sharee) throw redirect(301, '/console/shares')
+  const [{ id, schema, name }] = await sql<Entity[]>`select id, schema, name from entity where name=${entity} and project=${targetted_share.project}`
+  if (!id) throw redirect(301, '/console/shares')
+  const entries = await sql<Entry[]>`select * from entry where entity=${id} `
   return {
-    entries
+    entries,
+    schema,
+    sharer:targetted_share.sharer,
+    project:targetted_share.project_name,
+    entity:name,
+    share
   }
 }
--- src/routes/(console)/console/shares/[share]/[entity]/+page.svelte
+++ src/routes/(console)/console/shares/[share]/[entity]/+page.svelte
@@ -1,1 +1,63 @@
-<h1>Hello</h1>
+<script lang="ts">
+	import type { PageServerData } from './$types';
+	import { location, breadcrumb_items, show_create_entry } from '$lib/stores';
+	import Entry from '$lib/ui/Entry.svelte';
+	import ImagePreviewer from '$lib/ui/modals/ImagePreviewer.svelte';
+	location.set('/console/shares');
+	export let data: PageServerData;
+	$: ({ entries, schema, project, sharer, entity, share } = data);
+  $: console.log(project, entity)
+	$: fields = Object.entries(schema);
+  $: breadcrumb_items.set([
+      { title: `${sharer}'s ${project.toUpperCase()} `, path: `/console/shares/${share}` },
+      { title: entity, path: `/console/shares/${share}/${entity}` }
+  ]);
+</script>
+
+<svelte:head>
+	<title>YOOT | Shared entries</title>
+</svelte:head>
+
+<ImagePreviewer />
+
+<div class="w-full h-full bg-neutral-100 flex justify-center">
+	<div
+		class=" p-5 max-w-[600px] lg:max-w-[800px] 2xl:max-w-[1000px] h-full w-full flex flex-col justify-start transition-all gap-2"
+	>
+		<div class=" flex items-center justify-between">
+			<h1 class=" font-bold text-2xl">Entries</h1>
+			<button
+				on:click={() => {
+					show_create_entry.set(true);
+				}}
+				class=" bg-blue-400 p-2 rounded-full text-white w-32">New Entry</button
+			>
+		</div>
+		<div
+			class=" p-2 max-h-full w-full flex flex-col flex-wrap justify-start items-start gap-5 overflow-auto no-scroll"
+		>
+			<table
+				class=" max-w-[600px] lg:max-w-[800px] 2xl:max-w-[1000px] max-h-full w-full flex flex-col justify-start transition-all gap-5 overflow-auto"
+			>
+				<thead class="w-full">
+					<div
+						class=" flex items-center min-w-full border p-2 border-neutral-300 rounded-lg w-fit text-black h-10"
+					>
+						{#each fields as field}
+							<h1 class=" w-[300px] shrink-0">{field[0].toUpperCase()}</h1>
+						{/each}
+						<h1 class=" w-[300px] shrink-0">Actions</h1>
+					</div>
+				</thead>
+				<div class="w-full">
+					<tbody class="">
+						{#each entries as entry}
+							<Entry from_share={true} schema={data.schema} {entry} />
+							<hr />
+						{/each}
+					</tbody>
+				</div>
+			</table>
+		</div>
+	</div>
+</div>
