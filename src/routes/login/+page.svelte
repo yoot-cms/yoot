da1c8b66b0142eae0a82c5fb9f230bf48dad7a32
TheWisePigeon <zozozozeph@gmail.com>
2023-08-21 09:10:47 +0000

    fix: typo Register -> Login

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -158,7 +158,7 @@
 					class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 				>
 					{#if !loading}
-						<h1>Register</h1>
+						<h1>Login</h1>
 					{:else}
 						<Loading />
 					{/if}
