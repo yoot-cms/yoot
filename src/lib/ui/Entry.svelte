ab5d06a26e5a37d97fb007eb3deeb0f2b24cd8c0
TheWisePigeon <zozozozeph@gmail.com>
2023-08-28 11:28:21 +0000

    fix: boolean type always showing checked

--- src/lib/ui/Entry.svelte
+++ src/lib/ui/Entry.svelte
@@ -36,7 +36,7 @@
 		{:else if field_type === 'Boolean'}
 			<div class=" flex flex-col gap-2 truncate w-[300px] shrink-0">
 				<label class="relative inline-flex items-center cursor-pointer">
-					<input type="checkbox" disabled checked={entry.value[field_name]} class="sr-only peer" />
+					<input type="checkbox" disabled checked={JSON.parse(entry.value[field_name])} class="sr-only peer" />
 					<div
 						class="w-11 h-6 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"
 					/>
